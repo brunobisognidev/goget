30a56385f592084c3c4a35e72f7590e43f39fb9f
bruno bisogni <brunobisogni5@gmail.com>
2019-06-29 20:12:14 -0300

    nodemon

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const server = express();
+
+const users = ["Bruno", "Robertson", "Ismael"];
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
+});
+
+server.listen(3000);
