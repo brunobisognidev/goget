bcd530f5dd7ea53e505ed12ed9336a26c863528e
bruno bisogni <brunobisogni5@gmail.com>
2019-06-30 07:27:25 -0300

    CRUD Feito e funcional com json

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ server.use(express.json());
 
 const users = ["Bruno", "Robson", "Ismael"];
 
+server.use((req, res, next) => {
+  console.time("Request");
+  console.log(`Metódo: ${req.method}; URL: ${req.url}`);
+
+  next();
+
+  console.timeEnd("Request");
+});
+
+function checkUserExists(req, res, news) {
+  if (!req.body.use) {
+    return res
+      .status(400)
+      .json({ erro: "Usuário não encontrado no corpo da requisião" });
+  }
+}
+
 server.get("/users", (req, res) => {
   return res.json(users);
 });
@@ -31,6 +48,14 @@ server.put("/users/:index", (req, res) => {
   users[index] = name;
 
   return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.slice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
