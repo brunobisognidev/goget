51bd64050cb7a6f04337b864025c3a0a338194ef
bruno bisogni <brunobisogni5@gmail.com>
2019-06-30 02:00:47 -0300

    crud quase pronto

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ const express = require("express");
 
 const server = express();
 
-const users = ["Bruno", "Robertson", "Ismael"];
+server.use(express.json());
+
+const users = ["Bruno", "Robson", "Ismael"];
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
 
 server.get("/users/:index", (req, res) => {
   const { index } = req.params;
+
   return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
 });
 
 server.listen(3000);
